3a323e9607ef3c33065478f3099f4f2a43a28df9
Dan Roach <dan.roach@stgconsulting.com>
2016-09-26 15:27:12 -0600

    Added solution to challenge 1

---
+++ Challenge01/Challenge01/Challenge01/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace Challenge01
+{
+    public partial class MainPage : ContentPage
+    {
+        private ObservableCollection<Value> _valueList = new ObservableCollection<Value>();
+        public MainPage()
+        {
+            InitializeComponent();
+            ResultsView.ItemsSource = _valueList;
+        }
+
+        private void SubmitButton_OnClicked(object sender, EventArgs e)
+        {
+            _valueList.Add(new Value(InputEntry.Text));
+            InputEntry.Text = string.Empty;
+        }
+
+        private void SortButton_OnClicked(object sender, EventArgs e)
+        {
+            var sortedList = _valueList.OrderBy(x => x).ToList();
+            _valueList.Clear();
+            foreach (var value in sortedList)
+                _valueList.Add(value);
+        }
+    }
+}
---
+++ Challenge01/Challenge01/Challenge01/Value.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Challenge01
+{
+    public class Value : IComparable
+    {
+        private char letter;
+        private int number;
+
+        public Value(string input)
+        {
+            letter = input[0];
+            var numString = ""; // no sense using a StringBuilder, it's only two characters.
+            for (int i = 1; i < input.Length; i++)
+            {
+                numString += input[i];
+            }
+
+            number = int.Parse(numString);
+        }
+
+        public override string ToString()
+        {
+            return $"{letter}{number}";
+        }
+
+        public int CompareTo(object obj)
+        {
+            var other = obj as Value;
+            if (other == null)
+                return 0;
+
+            if (letter == other.letter)
+                return number - other.number;
+
+            return letter - other.letter;
+        }
+    }
+}
