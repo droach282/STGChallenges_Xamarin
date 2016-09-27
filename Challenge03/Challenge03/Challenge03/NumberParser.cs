18a3f8040d33aaa2950232d5970ec564b4ad1c8d
Dan Roach <dan.roach@stgconsulting.com>
2016-09-27 10:42:05 -0600

    Challenge 03

---
+++ Challenge03/Challenge03/Challenge03/NumberParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Challenge03
+{
+    public static class NumberParser
+    {
+        public static int Parse(string value)
+        {
+            var numbers = new List<decimal>();
+            var letters = 0;
+
+            foreach (var v in value)
+            {
+                if (char.IsLetter(v))
+                    letters++;
+                else if (char.IsNumber(v))
+                    numbers.Add(decimal.Parse("" + v));
+            }
+
+            return (int) Math.Round(numbers.Sum()/letters, MidpointRounding.AwayFromZero);
+        }
+    }
+}
