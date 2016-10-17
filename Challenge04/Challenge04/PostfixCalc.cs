70b7109cbd9d5ff8ebf3a0c73a6782cf34179838
Dan Roach <dan.roach@stgconsulting.com>
2016-10-17 15:48:54 -0600

    Added solution to challenge 4

---
+++ Challenge04/Challenge04/PostfixCalc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Challenge04
+{
+    public static class PostfixCalc
+    {
+        public static int Calculate(this string postfix)
+        {
+            var operandStack = new Stack<int>();
+
+            foreach (var c in postfix)
+            {
+                if (char.IsNumber(c))
+                    operandStack.Push(int.Parse($"{c}"));
+                else
+                {
+                    var operandR = operandStack.Pop();
+                    var operandL = operandStack.Pop();
+                    int result;
+
+                    switch (c)
+                    {
+                        case '+':
+                            result = operandL + operandR;
+                            break;
+
+                        case '-':
+                            result = operandL - operandR;
+                            break;
+
+                        case '*':
+                            result = operandL*operandR;
+                            break;
+
+                        case '/':
+                            result = operandL/operandR;
+                            break;
+
+                        default:
+                            throw new InvalidOperationException($"Unknown operation: {c}");
+                    }
+
+                    operandStack.Push(result);
+                }
+            }
+
+            if (operandStack.Count > 1)
+                throw new InvalidOperationException("Too many operands");
+
+            return operandStack.Pop();
+        }
+    }
+}
---
+++ Challenge04/Challenge04/PostfixParser.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Challenge04
+{
+    public static class PostfixParser
+    {
+        public static string ToPostfix(this string infix)
+        {
+            var operatorStack = new Stack<char>();
+            var outputQueue = new Queue<char>();
+            var operators = new Dictionary<char, short>
+            {
+                {'*', 3},
+                {'/', 3},
+                {'+', 2},
+                {'-', 2},
+                {'(', 1} // don't want to pop this prematurely.
+            };
+
+            for (var i = 0; i < infix.Length; i++)
+            {
+                var current = infix[i];
+
+                if (char.IsNumber(current))
+                {
+                    outputQueue.Enqueue(current);
+                    continue;
+                }
+
+                if (operators.ContainsKey(current) && operators[current] != 1) // skip '(', handled below.
+                {
+                    while (operatorStack.Count > 0 && operators[current] <= operators[operatorStack.Peek()])
+                        outputQueue.Enqueue(operatorStack.Pop());
+
+                    operatorStack.Push(current);
+                    continue;
+                }
+
+                if (current == '(')
+                {
+                    // handle inferred '*' operation
+                    if (i > 0 && (char.IsNumber(infix[i-1]) || infix[i-1] == ')'))
+                        operatorStack.Push('*');
+
+                    operatorStack.Push(current);
+                    continue;
+                }
+
+                if (current == ')')
+                {
+                    while (operatorStack.Peek() != '(')
+                        outputQueue.Enqueue(operatorStack.Pop());
+
+                    operatorStack.Pop(); // throw out '('
+                }
+            }
+
+            while (operatorStack.Count > 0)
+                outputQueue.Enqueue(operatorStack.Pop());
+
+            var sb = new StringBuilder();
+            while (outputQueue.Count > 0)
+                sb.Append(outputQueue.Dequeue());
+
+            return sb.ToString();
+        }
+    }
+}
